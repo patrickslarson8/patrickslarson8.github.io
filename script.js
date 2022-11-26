f63a80002b67e18a4df0e430983e94122d4fedbb
patrickslarson8 <pat.s.larson8@gmail.com>
2022-11-26 14:43:12 -0600

    Add ability to filter projects

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ home.onclick = function() {marker_style.top = "15px"}
 projects.onclick = function() {marker_style.top = "74px"}
 about.onclick = function() {marker_style.top = "135px"}
 background.onclick = function() {marker_style.top = "193px"}
+
+var csharpbtn = document.getElementById("csharpbtn")
+var cppbtn = document.getElementById("cppbtn")
+var pythonbtn = document.getElementById("pythonbtn")
+var sqlbtn = document.getElementById("sqlbtn")
+
+csharpbtn.addEventListener("click", (event) => {filterSelection(event, "c-sharp")});
+cppbtn.addEventListener("click", (event) => {filterSelection(event, "cpp")});
+pythonbtn.addEventListener("click", (event) => {filterSelection(event, "python")});
+sqlbtn.addEventListener("click", (event) => {filterSelection(event, "sql")});
+
+function filterSelection(event, term) {
+    var contentCells, callingBtn, i;
+    callingBtn = event.target;
+    contentCells = document.getElementsByClassName("content");
+
+    if(callingBtn.classList.contains("btn-active")) {
+        console.log("deactivating button");
+        callingBtn.classList.remove("btn-active");
+        for (i = 0; i < contentCells.length; i++) {
+            if (contentCells[i].classList.contains(term)) {
+              contentCells[i].classList.remove("content-active");
+            }
+          }
+    }
+    else {
+        console.log("activating button");
+        callingBtn.classList.add("btn-active");
+        for (i = 0; i < contentCells.length; i++) {
+            if (contentCells[i].classList.contains(term)) {
+            contentCells[i].classList.add("content-active");
+            }
+        }
+    }
+}
