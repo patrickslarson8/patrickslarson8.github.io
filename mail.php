83fc909fc8f07a399587865e1658b84ebcf475fa
patrickslarson8 <pat.s.larson8@gmail.com>
2022-12-03 14:50:03 -0600

    update mail.php

--- mail.php
+++ mail.php
@@ -1,4 +1,18 @@
 <?php
-echo '<script>alert("Hello")</script>';
+
 // PHP code goes here
+
+// if (isset($_POST['submit'])) {
+//     $name = $_POST['name'];
+//     $emailFrom = $_POST['email'];
+//     $subject = $_POST['subject'];
+//     $message = $_POST['message'];
+
+//     $mailTo = "pat.s.larson8@outlook.com";
+//     $headers = "From: ".$emailFrom;
+//     $txt = "E-mail from ".$name."\n\n".$message;
+
+//     mail($mailTo, $subject, $txt, $headers);
+//     header("Location: index.html?mail");
+// }
 ?>
